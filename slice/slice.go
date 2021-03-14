1a029afdc62d03f8126d46ce293aa7b50217e874
reekamaharaj <reekamaharaj@gmail.com>
2021-03-14 16:02:26 -0500

    starting slow and working on actually understaind what I'm coding hah. So far so good

--- slice/slice.go
+++ slice/slice.go
@@ -1,14 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	//program starts
+	fmt.Println("The slice sort program has started.")
+	//create an empty integer slice of length 3
+	// intSlice := make([]int, 3)
+
+	//loop start
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		//Tells program to scan input using bufio module, os: operating system, Stdin: standard input, what is input to command line. Creates object scanner.
+		fmt.Println("Enter a number to add to the slice. Hit X to exit program")
+		//Prompt user to enter integer
+		scanner.Scan()
+		//scan what the user input as a string
+		userInput := scanner.Text()
+		//Save the text scanned to the variable userInput
+
+		//check if x or X is entered and exit if yes
+		if userInput == "X" || userInput == "x" {
+			fmt.Println("You exited the program.")
+			break
+		} else {
+			fmt.Println("You have not exited the program")
+		}
+
+	}
 
 }
 
-//program starts
-//create an empty integer slice of length 3.
-
-//loop start.
-//Prompt user to enter integer
 //check if its an integer
 //x or X will exit program
 //if an integer is added then add it to the slice
@@ -22,3 +48,5 @@ func main() {
 //Does it compile
 //Does it print a sorted slice after every entry
 //Can more than three integers be added and sorted?
+
+//Should check that the entered value is a number, and if it isn't ask user for new input that is a number
