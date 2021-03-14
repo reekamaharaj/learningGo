673959ee7a57c75dbf37fd8c635e8949fcc28516
reekamaharaj <reekamaharaj@gmail.com>
2021-03-14 15:49:03 -0500

    reread directions and grading criteria and made new notes for execution

--- slice/slice.go
+++ slice/slice.go
@@ -1,33 +1,24 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	fmt.Println("Slice started. Hit Enter to continue or type X to exit.")
-	intSlice := make([]int, 3)
-	// intSlice := []int{1, 2, 3}
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	userInput := scanner.Text()
 
-	for i := range intSlice {
-		//i is the index
-		if userInput == "X" || userInput == "x" {
-			fmt.Println("You exited the program.")
-			break
-		}
-
-		var intEntered int
-		fmt.Println("Enter integers for storage, type X to exit")
-		fmt.Scanln(&intEntered)
-		intSlice[i] = intEntered
-	}
-
-	fmt.Printf("The slice you created has a length of %d a capacity of %d and the full slice is %v\n", len(intSlice), cap(intSlice), intSlice)
 }
 
-// Code works okay. Isn't perfect at all. x will exit before the loop has started and during. But doesn't return message that program is exited. Code should also check for values and send an err when a number isn't entered...It doesn't do that currently. Still needs work.
+//program starts
+//create an empty integer slice of length 3.
+
+//loop start.
+//Prompt user to enter integer
+//check if its an integer
+//x or X will exit program
+//if an integer is added then add it to the slice
+//sort the slice
+//print the sorted slice
+//ask for another integer
+//end loop. or continue with the loop.
+
+//slice has to grow in size if the user continues to add integers.
+
+//Does it compile
+//Does it print a sorted slice after every entry
+//Can more than three integers be added and sorted?
