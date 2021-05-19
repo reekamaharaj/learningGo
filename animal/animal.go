fb844771b9977979bcfc697a2a68022aa9456b2d
reekamaharaj <reekamaharaj@gmail.com>
2021-05-18 20:49:35 -0500

    animal.go completed

--- animal/animal.go
+++ animal/animal.go
@@ -1,10 +1,125 @@
-// Write a program which allows the user to get information about a predefined set of animals. Three animals are predefined, cow, bird, and snake. Each animal can eat, move, and speak. The user can issue a request to find out one of three things about an animal: 1) the food that it eats, 2) its method of locomotion, and 3) the sound it makes when it speaks. The following table contains the three animals and their associated data which should be hard-coded into your program.
+package main
 
-// Animal	Food eaten	Locomotion method	Spoken sound
-// cow	grass	walk	moo
-// bird	worms	fly	peep
-// snake	mice	slither	hsss
-// Your program should present the user with a prompt, “>”, to indicate that the user can type a request. Your program accepts one request at a time from the user, prints out the answer to the request, and prints out a new prompt. Your program should continue in this loop forever. Every request from the user must be a single line containing 2 strings. The first string is the name of an animal, either “cow”, “bird”, or “snake”. The second string is the name of the information requested about the animal, either “eat”, “move”, or “speak”. Your program should process each request by printing out the requested data.
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Animal struct {
+	name       string
+	food       string
+	locomotion string
+	noise      string
+}
+
+func main() {
+	//Cow struct
+	cow := Animal{
+		name:       "Cow",
+		food:       "Grass",
+		locomotion: "Walk",
+		noise:      "Moo",
+	}
+
+	//Bird struct
+	bird := Animal{
+		name:       "Bird",
+		food:       "Worms",
+		locomotion: "Fly",
+		noise:      "Peep",
+	}
+
+	//Snake struct
+	snake := Animal{
+		name:       "Snake",
+		food:       "Mice",
+		locomotion: "Slither",
+		noise:      "Hiss",
+	}
+	for {
+		fmt.Println("Choose an animal: Cow, Bird, or Snake.")
+		fmt.Println("Choose a topic: Food, Locomotion, or Noise.")
+		fmt.Println("Enter both. For a response about the animal.\n Example if you entered: Dog Noise \n You would get a response with 'Bark'.")
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		userInput := scanner.Text()
+		lowerString := strings.ToLower(userInput)
+		s := strings.Split(lowerString, " ")
+		animal, action := s[0], s[1]
+
+		switch animal {
+		case "cow":
+			if action == "food" {
+				fmt.Println("\n Response:", cow.food, "\n")
+			}
+
+			if action == "locomotion" {
+				fmt.Println("\n Response:", cow.locomotion, "\n")
+			}
+
+			if action == "noise" {
+				fmt.Println("\n Response:", cow.noise, "\n")
+			} else {
+				fmt.Println("\n Something went wrong. Please try again. \n")
+			}
+
+		case "bird":
+			if action == "food" {
+				fmt.Println("\n Response:", bird.food, "\n")
+			}
+
+			if action == "locomotion" {
+				fmt.Println("\n Response:", bird.locomotion, "\n")
+			}
+
+			if action == "noise" {
+				fmt.Println("\n Response:", bird.noise, "\n")
+			} else {
+				fmt.Println("\n Something went wrong. Please try again. \n")
+			}
+		case "snake":
+			if action == "food" {
+				fmt.Println("\n Response:", snake.food, "\n")
+			}
+
+			if action == "locomotion" {
+				fmt.Println("\n Response:", snake.locomotion, "\n")
+			}
+
+			if action == "noise" {
+				fmt.Println("\n Response:", snake.noise, "\n")
+			} else {
+				fmt.Println("\n Something went wrong. Please try again. \n")
+			}
+		default:
+			fmt.Println("\n Something went wrong. Please try again. \n")
+		}
+	}
+
+}
+
+// Get info about animals
+//Defined animals
+//Cow, bird, Snake
+//Defined actions
+//Eat, Move, Speak
+//Grass, Worms, Mice
+//Walk, Fly, Slither
+//Moo, Peep, Hiss
+
+//User can for information about animals
+//What food it eats
+//How it moves
+//What sound it makes
+
+//">" promt to user to type request
+//One request at a time with two strings
+//first string animal. second stringg action
+//returns the answer
+//loops forever
 
 // You will need a data structure to hold the information about each animal. Make a type called Animal which is a struct containing three fields:food, locomotion, and noise, all of which are strings. Make three methods called Eat(), Move(), and Speak(). The receiver type of all of your methods should be your Animal type. The Eat() method should print the animal’s food, the Move() method should print the animal’s locomotion, and the Speak() method should print the animal’s spoken sound. Your program should call the appropriate method when the user makes a request.
 
